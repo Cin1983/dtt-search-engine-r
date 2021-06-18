f57a445065389461aa057d8292c8e5e6ad03bd1d
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-06-18 23:35:47 +0200

    API_Key

---
+++ src/apis/api.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="button-wrapper">
+        <button class="btn" @click="searchUnsplash('Autumn')">Autumn</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+// searchUnsplash(topic);{
+this.images=[] ;
+    axios.get(
+        `https://api.unsplash.com/GET/photos/random?query={topic}&per_page=10`,
+
+        {
+        headers:{
+          Authorization:
+          "Client-ID oozTUUGcoOmOleTWzWgrDTU04NmhO8a-pxF5TmPPNKw",
+          "Accept-Version":'v1'
+         }    
+    }
+)    
+    .then(response => {
+          this.images = response.data.results;
+        })
+     
+            
+   
+
--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "semantic-ui-css/semantic.min.css";
+// import apis from './apis/api.vue';
 
 createApp(App).use(router).mount("#app");
