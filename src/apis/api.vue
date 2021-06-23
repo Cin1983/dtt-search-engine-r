bcbc35795e985cbf62638984dc46adb54f0a54fe
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-06-23 18:12:50 +0200

    api.vue

--- src/apis/api.vue
+++ src/apis/api.vue
@@ -15,28 +15,25 @@ import axios from "axios";
 export default {
   name: "api",
   methods: {
-searchUnsplash(topic){
-  if (!this.images) this.images=[]
-  axios.get(
-      `https://api.unsplash.com/photos/random?query=${topic}&per_page=10`,
+    searchUnsplash(topic) {
+      if (!this.images) this.images = [];
+      axios
+        .get(
+          `https://api.unsplash.com/photos/random?query=${topic}&per_page=10`,
 
-      {
-        headers: {
-          Authorization:
-            "Client-ID oozTUUGcoOmOleTWzWgrDTU04NmhO8a-pxF5TmPPNKw",
-          "Accept-Version": "v1",
-        },
-      }
-    )
-    .then((response) => {
-      this.images.push(response.data);
-      console.log(this.images);
-    });
-}
-}
-
-
-
+          {
+            headers: {
+              Authorization:
+                "Client-ID oozTUUGcoOmOleTWzWgrDTU04NmhO8a-pxF5TmPPNKw",
+              "Accept-Version": "v1",
+            },
+          }
+        )
+        .then((response) => {
+          this.images.push(response.data);
+          console.log(this.images);
+        });
+    },
+  },
 };
-
 </script>
